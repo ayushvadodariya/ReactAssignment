d739a549e3c481dbe72ac7943b4ee53c9478673d
ayush <ayushvadodariya12@gamil.com>
2025-02-12 19:47:34 +0530

    completed task3

---
+++ task3/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+       <footer>© 2025 ReactWdfAssignmets</footer>
+    </div>
+  )
+}
+
+export default Footer
---
+++ task3/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+    <h1>React-Wdf-Assignments</h1>
+    <nav>
+      <a href='/'>Home</a><br/>
+      <a href='/'>About</a><br/>
+      <a href='/'>Contact</a>
+    </nav>
+  </div>
+  )
+}
+
+export default Header
